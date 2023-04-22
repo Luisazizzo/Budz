5faeb172465bada49ce68f1dc73c798b9e973571
Luisazizzo <luisa0zizzo@gmail.com>
2023-04-22 12:24:06 +0200

    main project

---
+++ src/components/tendina_menu/Tendina_Menu.jsx
@@ -0,0 +1,39 @@
+import { BiFolder, BiHeart, BiShuffle, BiStar, BiUserX } from "react-icons/bi";
+import logo from "../../../public/logo/cuffie.png";
+import Image from "next/image";
+import styles from "./index.module.scss";
+import Link from "next/link";
+
+const Tendina_Menu = ({ tendina, onHandleLogout }) => {
+  return (
+    <div className={`${styles.Tendina_Menu} ${tendina && styles.show}`}>
+      <div className={styles.logo1}>
+        <Image src={logo} height={40} width={45} alt={"logo"} />
+        <BiUserX onClick={onHandleLogout} className={styles.icon} />
+      </div>
+
+      <div className={styles.content}>
+        <Link className={styles.box} href="/favouriteAlbum_page">
+          <BiFolder className={styles.icon} />
+          <p>Album</p>
+        </Link>
+
+        <Link className={styles.box} href="/favoriteTrack_page">
+          <BiHeart className={styles.icon} />
+          <p>Favorites</p>
+        </Link>
+
+        <Link className={styles.box} href="/shuffle_page">
+          <BiShuffle className={styles.icon} />
+          <p>Shuffle</p>
+        </Link>
+
+        <Link className={styles.box} href="/favoriteArtist_page">
+          <BiStar className={styles.icon} />
+          <p>Artist</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Tendina_Menu;
